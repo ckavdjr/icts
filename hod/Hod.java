c10da33cad9c3e0c6ade08a2f5975d7182cd19ba
ckavdjr <aravindharidas2000@gmail.com>
2021-12-14 21:22:32 +0530

    Added Hod class (Hari)

--- hod/Hod.java
+++ hod/Hod.java
@@ -1,9 +1,11 @@
+/**
+ * @author Harichand Manoj
+ */
+
 package hod;
 
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
-import java.awt.GridLayout;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 
@@ -42,27 +44,27 @@ public class Hod {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("Welcome");
 		lblNewLabel.setBounds(22, 21, 66, 27);
 		frame.getContentPane().add(lblNewLabel);
-		
+
 		JButton btnNewButton = new JButton("View Requests");
 		btnNewButton.setBounds(10, 77, 190, 35);
 		frame.getContentPane().add(btnNewButton);
-		
+
 		JButton btnNewButton_1 = new JButton("View Employee Details");
 		btnNewButton_1.setBounds(10, 123, 190, 36);
 		frame.getContentPane().add(btnNewButton_1);
-		
+
 		JButton btnNewButton_2 = new JButton("View Customer Details");
 		btnNewButton_2.setBounds(10, 170, 190, 34);
 		frame.getContentPane().add(btnNewButton_2);
-		
+
 		JButton btnNewButton_3 = new JButton("Assign Hod");
 		btnNewButton_3.setBounds(10, 215, 190, 35);
 		frame.getContentPane().add(btnNewButton_3);
-		
+
 		JButton btnNewButton_4 = new JButton("Log Out");
 		btnNewButton_4.setBounds(323, 11, 89, 23);
 		frame.getContentPane().add(btnNewButton_4);
